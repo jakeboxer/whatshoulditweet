6dc6cdbbe52c1d46549f3b5b304e78e051b42a9b
Jake Boxer <jake@github.com>
2013-05-25 22:11:16 -0700

    Exclude RTs from Twitter timeline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @username = params[:id]
-    @tweets   = Twitter.user_timeline(@username, :count => 200)
+    @tweets   = Twitter.user_timeline(@username, :count => 200, :include_rts => false)
 
     generator = TwoChainz::Generator.new(:boring => true)
 
