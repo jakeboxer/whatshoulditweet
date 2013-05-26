d8e9478dcb834186ac1159cd6701a262f9ae9908
Jake Boxer <jake@github.com>
2013-05-25 18:22:46 -0700

    Add Users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Whatshoulditweet::Application.routes.draw do
-  resources :users, :only => [:show]
+  resources :users, :only => [:index, :show]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
   test "should get show" do
     get :show, :id => 'irrelevant'
     assert_response :success
