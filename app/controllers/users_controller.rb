c0728915e1698d3b51283818944175e0db8289fb
Jake Boxer <jake@github.com>
2013-05-26 00:02:54 -0700

    Trim user on Twitter API request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @username = params[:id]
-    @tweets   = Twitter.user_timeline(@username, :count => 200, :include_rts => false)
+    @tweets   = Twitter.user_timeline(@username, :count => 200, :include_rts => false, :trim_user => true)
 
     generator = TwoChainz::Generator.new(:boring => true)
 
